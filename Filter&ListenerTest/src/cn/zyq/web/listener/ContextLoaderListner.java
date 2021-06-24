e4fb7eacb1bb35f5479e3ce5262df856443a1ddc
zengyuqiang <694319924@qq.com>
2021-06-24 22:59:46 +0800

    监听器

---
+++ Filter&ListenerTest/src/cn/zyq/web/listener/ContextLoaderListner.java
@@ -0,0 +1,16 @@
+package cn.zyq.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextLoaderListner implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
