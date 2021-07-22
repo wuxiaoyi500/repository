c7e4d0102f8c1d9372d4f9e3264057216121235a
zengyuqiang <694319924@qq.com>
2021-07-22 11:27:17 +0800

    123

--- MavenTest/src/main/java/cn/zyq/helloMaven.java
+++ MavenTest/src/main/java/cn/zyq/helloMaven.java
@@ -1,12 +1,13 @@
 package cn.zyq;
 
 public class helloMaven {
-    public static int addNumber(int n1,int n2){
+    public static int addNumber(int n1,int n2) {
         return n1+n2;
     }
-    public static void main(String[] args) {
-        helloMaven helloMaven = new helloMaven();
-        int result = helloMaven.addNumber(10, 20);
-        System.out.println("result = "+ result);
+    public static void main(String args[]){
+        helloMaven hm = new helloMaven();
+        int result = hm.addNumber(10, 20);
+        System.out.println(result   );
     }
+
 }
--- MavenTest/src/test/java/cn/zyq/testHelloMaven.java
+++ MavenTest/src/test/java/cn/zyq/testHelloMaven.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 
 public class testHelloMaven {
     @Test
-    public void testAddNumber(){
-        helloMaven helloMaven = new helloMaven();
-        int result = helloMaven.addNumber(10, 20);
-        System.out.println(result);
+    public void test1(){
+        System.out.println("test111111111");
+        helloMaven hm = new helloMaven();
+        int result = hm.addNumber(10, 20);
         Assert.assertEquals(30,result);
+    }
+    @Test
+    public void test2(){
+        System.out.println("test22222222");
+        helloMaven hm = new helloMaven();
+        int result = hm.addNumber(10, 20);
+        Assert.assertEquals(3,result);
     }
 }
